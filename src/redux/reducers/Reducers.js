8809885281c193ec5f7b4e2ef8527d9e58326218
ritik-gupta3010 <ritikgupta30102000@gmail.com>
2023-01-30 04:30:16 +0530

    fetch bottom data updated

--- src/redux/reducers/Reducers.js
+++ src/redux/reducers/Reducers.js
@@ -1,14 +1,25 @@
 const initialState={
-    top:[],
-    bottom:[]
+    top:{},
+    bottom:{},
+    currentPageIndex:0,
+    limit:50
 }
 
 const reducer=(state=initialState,action)=>{
+
     switch (action.type) {
         case "FETCHTOPDATA":
             return {...state,top:action.data}
         case "FETCHBOTTOMDATA":
-            return {...state,bottom:action.data}
+            console.log(action.data)
+            const {bottom}=state;
+            const updatedBottomData=Object.assign({},bottom);
+            let currentPage=action.data.data.current;
+            updatedBottomData[currentPage]=action.data.data;
+            console.log(updatedBottomData)
+            return {...state,bottom:updatedBottomData}
+        case "SET_DATA_LIMIT":
+            return {...state,limit:action.data}
         default:
             return state
     }
