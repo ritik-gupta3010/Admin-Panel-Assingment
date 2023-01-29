39afee7edb9c43990acadbb526ef2fca26a208cc
ritik-gupta3010 <ritikgupta30102000@gmail.com>
2023-01-30 04:31:28 +0530

    page limit

--- src/redux/actions/Action.js
+++ src/redux/actions/Action.js
@@ -17,10 +17,11 @@ export const fetchTopData=()=>{
             })
     }
 }
-export const fetchBottomData=()=>{
-    return (dispatch)=>{
-        axios.get(`https://admindevapi.wowtalent.live/api/admin/dashboard/installstatasticlist?fromdate=2022-04-01&todate=2022-08-24&page=1&limit=10 
-        `)
+
+export const fetchBottomData=(currentPage)=>(dispatch,getState)=>{
+    const{limit}=getState();
+    console.log(getState())
+        axios.get(`https://admindevapi.wowtalent.live/api/admin/dashboard/installstatasticlist?fromdate=2022-04-01&todate=2022-08-24&page=${currentPage}&limit=${limit}`)
             .then(response =>{
                 dispatch({
                     type: "FETCHBOTTOMDATA",
@@ -33,5 +34,29 @@ export const fetchBottomData=()=>{
                     msg: "Unable to fetch data" 
                 })
             })
+    }
+export const setDataLimit=(limit)=>{
+    // console.log(limit)
+    return dispatch=>{
+        dispatch({
+            type:'SET_DATA_LIMIT',
+            data:limit
+        })
+    }
+}
+export const setStartDate=(startDate)=>{
+    return dispatch=>{
+        dispatch({
+            type:'SET_START_DATE',
+            data:startDate
+        })
+    }
+}
+export const setEndDate=(endDate)=>{
+    return dispatch=>{
+        dispatch({
+            type:'SET_END_DATE',
+            data:endDate
+        })
     }
 }
