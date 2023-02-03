b44638007b5dbf7e5792738883e072d490a83ec7
ritik-gupta3010 <ritikgupta30102000@gmail.com>
2023-02-03 15:43:51 +0530

    bottom Nav

--- src/components/Bottomnav.js
+++ src/components/Bottomnav.js
@@ -4,15 +4,15 @@ export class Bottomnav extends Component {
   render() {
     return (
       <>
-        <div className="nav">
-            <div className="navItem1">Date</div>
-            <div className="navItem2">Day Installs</div>
-            <div className="navItem">Plateform</div>
-            <div className="navItem">Day Un-Installs</div>
-            <div className="navItem">Plateform</div>
-            <div className="navItem">Churn Rate</div>
-            <div className="navItem">Churn Plateform</div>
-        </div>
+        <ul className="nav">
+            <li className="navItem">Date</li>
+            <li className="navItem">Day Installs</li>
+            <li className="navItem">Plateform</li>
+            <li className="navItem">Day Un-Installs</li>
+            <li className="navItem">Plateform</li>
+            <li className="navItem">Churn Rate</li>
+            <li className="navItem">Churn Plateform</li>
+        </ul>
       </>
     )
   }
